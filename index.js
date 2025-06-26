47abc1fcce55c0e1b07e15ca29f2cd74071eac68
Micheni58 <michenicaleb@gmail.com>
2025-06-26 15:53:46 +0300

    Modify index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 const myApi = 'https://opensky-network.org/api/states/all';
+function fetchAPi(){
+    fetch('https://opensky-network.org/api/states/all')
+    .then((res) => res.json())
+    .then((data) => displayInfo(data))
+    
+}
+function displayInfo(planeData){
+    const list = document.createElement('li')
+    list.innerText = planeData;
+    const listItem = document.getElementsByClassName('plane-data')    
+    listItem.appendChild(li)
+}
