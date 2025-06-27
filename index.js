2ca5e759691629996d3326474d526d61ee4cbe5c
Micheni58 <michenicaleb@gmail.com>
2025-06-27 11:01:53 +0300

    Modify Search Logic

--- index.js
+++ index.js
@@ -12,6 +12,7 @@
 //     card.appendChild(li)
 // }
 // fetchAPi()
+let planeDataCache = null;
 function fetchAPi() {
   fetch('https://opensky-network.org/api/states/all')
     .then((res) => res.json())
@@ -33,11 +34,43 @@ function displayInfo(planeData) {
 }
 function searchData(){
     const searchButton = document.querySelector('#btnSearch')
-    // console.log(searchButton)
-    searchButton.addEventListener('click',(e)=>{
-       const value=  e.target.value;
-        console.log(value)
+    const searchInput = document.querySelector('#flightSearch')
+    const resultList =document.querySelector('.result-list')
+    // console.log(searchInput)
+    searchButton.addEventListener('click',(e) =>{
+    const callSign = searchInput.value.trim().toLowerCase();
+        if(!callSign){
+            resultList.innerHTML = '<li> Please enter a callsign to search </li>';
+            return;
+        }
+        if(!planeDataCache){
+            resultList.innerHTML = '<li> No flight data available.Pleas wait for data to load!</li>'
+            return;
+        }
+        const flight = planeDataCache.states.find(flight =>
+            flight[1]?.trim().toLowerCase() ===callSign
+        );
+        resultList.innerHTML = '';
+        if(flight){
+            const li = document.createElement('li');
+            li.innerHTML = `
+            CallSign: ${flight[1] || 'Unknown'}<br>
+            Origin COuntry:${flight[2] || 'Unknown'}<br>
+            Longitude:${flight[5] || 'N/A'}<br>
+            Latitude: ${flight[6] || 'N/A'}<br>
+            Altitude:${flight[7] || 'N/A'} m
+            `;
+            resultList.appendChild(li);
+        }else{
+            resultList.innerHTML = '<li>No flight found with that callsign</li>';
+        }
     })
-}
 
+
+}
+const clearButton = document.querySelector('#btnClear');
+clearButton.addEventListener('click', ()=>{
+    searchInput.value ='';
+    resultList.innerHTML = '';
+})
 fetchAPi();
